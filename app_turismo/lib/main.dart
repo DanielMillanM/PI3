bacefa6953d2583603c041d729c24ca22f5f6514
DanielMillanM <danielfmillanm@hotmail.com>
2024-04-08 14:36:54 -0500

    minor changes

--- app_turismo/lib/main.dart
+++ app_turismo/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'BucaraTismo',
       debugShowCheckedModeBanner: false,
-      home: MyMainPage(),
+      home: BodyBuild(),
       routes: {
         '/main': (context) => MyMainPage(),
         '/login': (context) => MyLoginPage(),
